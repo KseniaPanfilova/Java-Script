98ef29b5560d2448aa08d41226855f7175b7e091
kpanfilova <kseniapanfilova@gmail.com>
2022-06-18 14:11:29 +0300

    add solutions to tasks

--- Ksyushitsa_JS_HW3.js
+++ Ksyushitsa_JS_HW3.js
@@ -2,7 +2,7 @@
 console.log("\n-----------ex 1-----------\n");
 
 let x = 2;
-for (let i = 0; i <= 10; i = ++i) {
+for (let i = 1; i <= 10; i = ++i) {
     let a;
     console.log("2 в степени", i,"=", a = x ** i);
 }
@@ -12,12 +12,12 @@ console.log("\n-----------ex 1*-----------\n");
 
 let y = 2;
 function exp(exponent) {
-    for (let i = 0; i <= exponent; i = ++i) {
+    for (let i = 1; i <= exponent; i = ++i) {
         console.log("2 в степени", i,"=", a = y ** i);
     }
 }
 
-exp(3);
+exp(8);
 
 
 // 2. Написать скрипт, который выведет 5 строк в консоль таким образом, чтобы в первой строчке выводилось :), во второй :):) и так далее
@@ -40,6 +40,9 @@ for (let i = 1; i <= 5; i = ++i) {
 console.log("\n-----------ex 2*-----------\n");
 
 function moreSmilesForSmileGod(smile, smilesAmount) {
+    if (typeof smile != "string") {
+        smile = smile.toString();
+    }
     for (let i = 1; i <= smilesAmount; i = ++i) {
         console.log(smile.repeat(i));
     }
@@ -56,10 +59,55 @@ moreSmilesForSmileGod("=P", 7);
 console.log("\n-----------ex 3**-----------\n");
 
 function getWordStructure(word) {
-    
+    let vowels = "AEIOUАЕЁИОУЫЭЮЯ";
+    let consonants = "BCDFGHJKLMNPQRSTVWXYZБВГДЖЗЙКЛМНПРСТФХЦЧШЩЬЪ";
+    let vowelsCounter = 0;
+    let consonantsCounter = 0;
+    if (typeof(word) != "string" || ((typeof(+word) == "number" && !isNaN(+word)))) {
+        return(console.log("Данная функция обрабатывает только слова. Введите, пожалуйста, слово."));
+    } else {
+        for (let i = 0; i < word.length; i = ++i) {
+            for (let j = 0; j < vowels.length; j = ++j) {
+                if (word[i].toUpperCase() == vowels[j]) {
+                    vowelsCounter = ++vowelsCounter;
+                }
+                if (word[i].toUpperCase() == consonants[j]) {
+                    consonantsCounter = ++consonantsCounter;
+                }
+            }
+        }    
+        console.log("Слово", word, "состоит из", vowelsCounter, "гласных букв и", consonantsCounter, "согласных букв.");
+    }
 }
+     
+getWordStructure("1111");
 
 // 4**. Написать функцию, которая проверяет, является ли слово палиндромом
 // e.g. function isPalindrom(word)
-
 // Проверки: 'abba', 'Abba'
+
+console.log("\n-----------ex 4**-----------\n");
+
+function isPalindrom(str) {
+    if (typeof str != "string") {
+        return(console.log("Данная функция обрабатывает только строки. Введите строку."));
+    } else {
+        let strLow = str.toLowerCase();
+        let strWithoutSpaces = strLow.match(/[^\s]/g);
+        let counter = 0;
+        for (let i = 0; i < strWithoutSpaces.length; i = ++i) {
+            if (strWithoutSpaces[i] == strWithoutSpaces.reverse()[i]) {
+                counter = ++counter;
+            }
+        }
+        if (strWithoutSpaces.length == counter) {
+            console.log("Слово/фраза","'" + str + "'", "является полиндромом.");
+        } else {
+            console.log("Слово/фраза","'" + str + "'", "не является полиндромом.");
+        }
+    }
+}
+
+isPalindrom("Abba");
+
+
